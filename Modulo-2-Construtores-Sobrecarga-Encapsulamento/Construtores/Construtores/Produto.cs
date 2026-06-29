6be805137bbd181c602e4b75167de6ed0bd3663c
Emerson Oliveira <emerson.oliverbr@gmail.com>
2026-06-29 19:17:35 -0300

    Aprendendo Constutores

---
+++ Modulo-2-Construtores-Sobrecarga-Encapsulamento/Construtores/Construtores/Produto.cs
@@ -0,0 +1,36 @@
+using System.Globalization;
+
+namespace ControleDeEstoque;
+public class Produto
+{
+    public string? Nome;
+    public double Preco;
+    public int Quantidade;
+
+    public Produto(string nome, double preco, int quantidade)
+    {
+        Nome = nome;
+        Preco = preco;
+        Quantidade = quantidade;
+    }
+
+    public double ValorTotalEmEstoque()
+    {
+        return Quantidade * Preco;
+    }
+    
+    public void AdicionarProdutos(int quantidade)
+    {
+        Quantidade += quantidade;
+    }
+    
+    public void RemoverProdutos(int quantidade)
+    {
+        Quantidade -= quantidade;
+    }
+    public override string ToString()
+    {
+        return $"{Nome}, R$ preço R$ {Preco.ToString("F2",CultureInfo.InvariantCulture)}, " +
+               $"Quantidade: {Quantidade}, Valor total em estoque {ValorTotalEmEstoque().ToString("F2",CultureInfo.InvariantCulture)}";
+    }
+}
